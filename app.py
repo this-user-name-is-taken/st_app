76595ae4115c00a834129d55d8fc58f13ea71243
Dauren Polatbekov <dauren.polatbekov+github@gmail.com>
2023-05-22 14:09:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,69 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Function to preprocess the input image
+def preprocess_image(image):
+    # Resize the image to the desired input shape (e.g., 5000x5000)
+    resized_image = image.resize((5000, 5000))
+    # Perform any other preprocessing steps if needed
+    # ...
+    return resized_image
+
+# Function to predict the segmentation mask using your trained UNet model
+def predict_mask(image):
+    # Perform any necessary preprocessing on the image
+    preprocessed_image = preprocess_image(image)
+    
+    # Convert the image to an array and normalize the RGB values
+    image_array = np.array(preprocessed_image) / 255.0
+    
+    # Use your trained UNet model to predict the segmentation mask
+    # ...
+    # Replace the following line with your actual prediction code
+    predicted_mask = np.random.randint(0, 2, size=(5000, 5000))
+    
+    return predicted_mask
+
+# Streamlit app code
+def main():
+    st.title("Rooftop Segmentation App")
+
+    # Upload image
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        st.subheader("Uploaded Image")
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Perform prediction and get the segmentation mask
+        predicted_mask = predict_mask(image)
+
+        # Display the segmentation mask side by side with the uploaded image
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        axes[0].imshow(image)
+        axes[0].set_title("Original Image")
+        axes[0].axis("off")
+        axes[1].imshow(predicted_mask, cmap="gray")
+        axes[1].set_title("Segmentation Mask")
+        axes[1].axis("off")
+
+        # Show the figure in Streamlit
+        st.subheader("Segmentation Result")
+        st.pyplot(fig)
+
+        # Calculate and display the metrics (IoU, accuracy)
+        # ...
+        # Replace the following lines with your actual metric calculations
+        iou = np.random.rand()
+        accuracy = np.random.rand()
+        st.subheader("Metrics")
+        st.write("IoU:", iou)
+        st.write("Accuracy:", accuracy)
+
+if __name__ == "__main__":
+    main()
 
